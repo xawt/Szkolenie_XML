04e522c1d2247901952aa6c1cc87898ee6b89833
Patryk Czarnik <mail@patrykczarnik.net>
2017-10-16 11:52:32 +0200

    Programik do wypisywania NS

---
+++ src/xml/WypiszNs.java
@@ -0,0 +1,45 @@
+package xml;
+
+import java.io.File;
+
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+public class WypiszNs {
+
+	public static void main(String[] args) {
+		String plik = "wersja_prowadzacego/02-przestrzenie_nazw/ns1.xml";
+		
+		try {
+			SAXParserFactory spf = SAXParserFactory.newInstance();
+			spf.setNamespaceAware(true);
+			SAXParser saxParser = spf.newSAXParser();
+			InfoHandler handler = new InfoHandler();
+			saxParser.parse(new File(plik), handler);
+			System.out.println("Gotowe");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	private static class InfoHandler extends DefaultHandler {
+		@Override
+		public void startElement(String uri, String localName, String qName, Attributes attributes)
+				throws SAXException {
+			
+			System.out.println(uri + " : " + localName);
+		}
+		
+		@Override
+		public void endElement(String uri, String localName, String qName) throws SAXException {
+			System.out.println("/" + uri + " : " + localName);
+		}
+	}
+	
+
+}
